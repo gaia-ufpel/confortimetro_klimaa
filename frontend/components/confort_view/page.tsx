a625cb47dae7ab552aac7a8ff197493ac731d402
Alejandro Alberoni <atralberoni@inf.ufpel.edu.br>
2024-03-24 02:07:10 -0300

    added styles for metric selectors, preparing for grpahics

--- frontend/components/confort_view/page.tsx
+++ frontend/components/confort_view/page.tsx
@@ -7,7 +7,7 @@ const CONFORT_VIEW = (props: any) => {
     var startid = `START_DATE`;
     var endid = `END_DATE`;
     const [date, setDate] = useState({ [startid]: null, [endid]: null })
-    var [selectedParam, setSelectedParam] = useState(null)
+    const [clickedButtons, setClickedButtons] = useState([]);
     const params = ['Temperatura', 'Temperatura de globo', 'Pressão atmosférica', 'Umidade', 'Velocidade do vento']
     function fromContextSetDates(id: string, selector_date: any) {
         if (id == startid) {
@@ -17,9 +17,18 @@ const CONFORT_VIEW = (props: any) => {
             setDate({ ...date, [endid]: selector_date })
         }
     }
+    const handleClick = (option: never) => {
+        const index = clickedButtons.indexOf(option);
+        if (index === -1) {
+            setClickedButtons([...clickedButtons, option]);
+        } else {
+            const newButtons = [...clickedButtons];
+            newButtons.splice(index, 1);
+            setClickedButtons(newButtons);
+        }
+    };
     useEffect(() => {
-        console.log(date)
-    }, [date])
+    }, [date, clickedButtons])
     return (
         <div className='relative flex flex-col'>
             <DATE_CONTROL getState={fromContextSetDates} startid={startid} endid={endid}></DATE_CONTROL>
@@ -29,7 +38,7 @@ const CONFORT_VIEW = (props: any) => {
             </div>
             <div className='flex flex-col text-center md:flex-row  justify-center space-x-10'>
                 {
-                    params.map((value, index) => <button className='border-2 border-black' key={index} onClick={() => { }}>{value}</button>)
+                    params.map((value, index) => <button className='bg-green-500 hover:bg-green-600 text-white font-bold py-2 px-4 rounded flex items-center' key={index} onClick={() => { handleClick(value) }}>{value}</button>)
                 }
             </div>
 
