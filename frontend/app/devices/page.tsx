ef59579d04000573adc601527ea07dd420114012
Alejandro Alberoni <atralberoni@inf.ufpel.edu.br>
2024-04-18 19:10:29 -0300

    still fixing devices

--- frontend/app/devices/page.tsx
+++ frontend/app/devices/page.tsx
@@ -1,5 +1,5 @@
 "use client";
-import React, { useState, useEffect } from 'react'
+import React, { useState, useEffect, useRef } from 'react'
 import Loading_animation from '@/components/loading_animation';
 
 interface Device {
@@ -10,6 +10,10 @@ export default function DEVICES() {
     const [devices, setDevices] = useState<Device[]>([])
     const [isLoading, setIsLoading] = useState(false);
     const [error, setError] = useState(false);
+    const abortControllerRef = useRef<AbortController | null>(null);
+
+    const signal = abortControllerRef.current?.signal;
+
 
     const getDevices = async () => {
         var serialnumber;
@@ -17,16 +21,37 @@ export default function DEVICES() {
         var building;
         var room;
         const devicesurl = '/api/v1/devices';
+        var data;
+        abortControllerRef.current?.abort();
+        abortControllerRef.current = new AbortController();
+
+        setIsLoading(true);
+        try {
+            data = await fetch(`${devicesurl}`, { method: 'GET', signal: abortControllerRef.current?.signal })
+            const deviceData = (await data.json()) as Device[]
+            setDevices(deviceData)
+            setIsLoading(false);
+        } catch (e: any) {
+            if (e.name === 'AbortError') {
+                console.log('Fetch aborted');
+                return;
+            }
+            setError(e)
+        } finally {
+            setIsLoading(false);
+        }
     }
     return (
         <div className='relative flex justify-center min-h-screen items-center'>
+
             <button className='absolute top-0 right-0 m-10' onClick={getDevices}>
                 <p className="bg-yellow-500 hover:bg-yellow-600 text-white font-bold py-2 px-4 rounded">SEARCH DEVICES</p>
             </button>
             {isLoading && <Loading_animation />}
             <div className='flex md:flex-col'>
                 {
-                    devices.map((value, index) => <button className='' key={index}>{`${value}`}</button>) && <div className='font-sans text-lg'>NO DEVICES FOUND, PLEASE TRY AGAIN</div>
+                    devices.length > 0 ? devices.map((value, index) => <button className='bg-white text-black border border-gray-300 rounded-md px-4 py-2 m-2 transition duration-500 ease select-none hover:bg-gray-300 focus:outline-none focus:shadow-outline' key={index}>{value}</button>)
+                        : <div className="bg-yellow-100 border-l-4 border-yellow-500 text-yellow-700 p-4" role='alert'>No devices found</div>
                 }
             </div>
         </div>
