2095ffb64ce14fa5c04c3572d0de5c27b3901adc
Gabriel Leite Bessa <gabrielleitebessa@gmail.com>
2024-04-24 10:35:27 -0300

    Adding requirements.txt

--- backend/utils/__init__.py
+++
@@ -1,8 +0,0 @@
-import os
-
-SECRET_KEY = os.getenv("SECRET_KEY", "09d25e094faa6ca2556c818166b7a9563b93f7099f6f0f4caa6cf63b88e8d3e7")
-ALGORITHM = "HS256"
-ACCESS_TOKEN_EXPIRE_MINUTES = int(os.getenv("TOKEN_EXPIRE_MINUTES", "30"))
-DATABASE_URL = os.getenv("DATABASE_URL", "postgresql://gabriellb:gabriellb@localhost:5433/confortimetro_klimaa")
-HOST = os.getenv("HOST", "localhost")
-PORT = os.getenv("PORT", 8000)
