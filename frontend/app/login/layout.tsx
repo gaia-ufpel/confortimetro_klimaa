bb1334a3083beb969f70cdd766553bc6c30c7393
Alejandro Alberoni <atralberoni@inf.ufpel.edu.br>
2024-05-08 22:04:33 -0300

    ,,,

---
+++ frontend/app/login/layout.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+import LOGIN from './page'
+const layout = () => {
+  return (
+    <div className='relative bg-gradient-to-b from-[#41D271] to-[#BD95EB] min-h-screen min-w-screen'>
+        <LOGIN></LOGIN>
+    </div>
+  )
+}
+
+export default layout
--- frontend/app/registrar/page.tsx
+++ frontend/app/registrar/page.tsx
@@ -1,5 +1,5 @@
-import React from 'react'
-import { MdLock } from 'react-icons/md'
+import React from 'react';
+import { MdLock } from 'react-icons/md';
 const REGISTRAR = () => {
     return (
         <div>
@@ -7,25 +7,24 @@ const REGISTRAR = () => {
                 <div className='grid space-y-4'>
 
                     <div className='space-y-1'>
-                        <label className='block text-neutral-700 font-2xl font-normal pl-3'>Nome completo</label>
-                        <input className="w-[710px] h-16 bg-zinc-300 rounded-3xl shadow" type='text'></input>
+                        <label className='block text-neutral-700 text-[24px] font-normal pl-3'>Nome completo</label>
+                        <input className=" pl-8 pr-10 w-[710px] h-16 bg-zinc-300 rounded-3xl shadow-[0px_4px_4px_#00000040]" type='text'></input>
                     </div>
                     <div className='space-y-1'>
-                        <label className='block text-neutral-700 font-2xl font-normal pl-3'>E-mail</label>
-                        <input className="w-[710px] h-16 bg-zinc-300 rounded-3xl shadow" type='email'></input>
+                        <label className='block text-neutral-700 text-[24px] font-normal pl-3'>E-mail</label>
+                        <input className=" pl-8 pr-10 w-[710px] h-16 bg-zinc-300 rounded-3xl shadow-[0px_4px_4px_#00000040]" type='email'></input>
                     </div>
                     <div className='space-y-1'>
-                        <label className='block text-neutral-700 font-2xl font-normal pl-3'>Senha</label>
+                        <label className='block text-neutral-700 text-[24px] font-normal pl-3'>Senha</label>
                         <div className='relative flex items-center'>
-                            <input className=" pl-3 w-[710px] h-16 bg-zinc-300 rounded-3xl shadow" type='password'></input>
+                            <input className=" pl-8 pr-10 w-[710px] h-16 bg-zinc-300 rounded-3xl shadow-[0px_4px_4px_#00000040]" type='password'></input>
                             <MdLock className='absolute text-stone-500 right-6 pointer-events-none h-7 w-7' />
                         </div>
-
                     </div>
                     <div className='space-y-1'>
-                        <label className='block text-neutral-700 font-2xl font-normal pl-3'>Confirme sua senha</label>
+                        <label className='block text-neutral-700 text-[24px] font-normal pl-3'>Confirme sua senha</label>
                         <div className='relative flex items-center'>
-                            <input className="pl-3 w-[710px] h-16 bg-zinc-300 rounded-3xl shadow" type='password'></input>
+                            <input className="pl-8 pr-10 w-[710px] h-16 bg-zinc-300 rounded-3xl shadow-[0px_4px_4px_#00000040]" type='password'></input>
                             <MdLock className='absolute text-stone-500 right-6 pointer-events-none h-7 w-7' />
                         </div>
 
